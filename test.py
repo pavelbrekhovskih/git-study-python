d4627d4a3af526275faf73d5261dd6eb394f48c8
Pavel Brekhovskih <pasha.kai.89@gmail.com>
2022-12-03 10:09:40 +0300

    add_title

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+print('Факториал')
 def sum_factorials(num):
     part_factorial = 1
     summa = 0
@@ -6,6 +7,7 @@ def sum_factorials(num):
         part_factorial *= i
         summa += part_factorial
     return summa
+
 
 n = int(input("Введите число: "))
 print(sum_factorials(n))
