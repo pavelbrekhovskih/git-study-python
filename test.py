2a2a43da78bfc7b4b371294455c43619361ff86c
Pavel Brekhovskih <pasha.kai.89@gmail.com>
2022-12-03 10:38:57 +0300

    spec_chifir_added

--- test.py
+++ test.py
@@ -11,3 +11,19 @@ def sum_factorials(num):
 
 n = int(input("Введите число: "))
 print(sum_factorials(n))
+
+print('Задача 6. Спецшифр_1')      #Задачи на максимум решать со сравнения с 0
+text = input('Введите строку: ') #ssbbbsssbc sssbbbssbc  sbbbssssbbbbs sssbcbcbcbc aaassaabbs
+s_count = 0 # текущее количество s
+s_count_max = 0 # максимальное количество s
+
+for symb in text:
+    if symb == 's':
+        s_count += 1
+    else:
+        s_count = 0
+    if s_count > s_count_max:
+        s_count_max = s_count
+
+print('Самая длинная последовательность: ', s_count_max)
+print("-----------------------------------------------------------------------------------")
