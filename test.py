d9005bff9f3a20b8abfd77c7a81ae928b35f1499
Pavel Brekhovskih <pasha.kai.89@gmail.com>
2022-12-03 10:03:30 +0300

    initial

---
+++ test.py
@@ -0,0 +1,11 @@
+def sum_factorials(num):
+    part_factorial = 1
+    summa = 0
+
+    for i in range(1, num + 1):
+        part_factorial *= i
+        summa += part_factorial
+    return summa
+
+n = int(input("Введите число: "))
+print(sum_factorials(n))
